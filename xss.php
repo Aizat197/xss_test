1c103ddf6a227d7ee541921a82e657c367ff7dd5
Aizat197 <68383520+Aizat197@users.noreply.github.com>
2021-03-22 16:27:02 +0800

    Update xss.php

--- xss.php
+++ xss.php
@@ -1,4 +1,3 @@
-<?xml version="1.0" encoding="utf-8"?>
-<svg onload="alert(1)" version="1.1" id="mapplic-usa" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"
-	 y="0px" width="960px" height="600px" viewBox="0 0 960 600" enable-background="new 0 0 960 600" xml:space="preserve">
-</svg>
+<?php
+echo "<script>alert(1)</script>";
+?>
